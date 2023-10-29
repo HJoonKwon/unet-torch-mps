e67f57fc33f5aaf82649f8cf90ea8bdada4c06b4
HJoonKwon <dsazz801@gmail.com>
2023-10-29 18:09:16 +0900

    update: assembly blocks

--- model/unet.py
+++ model/unet.py
@@ -46,60 +46,45 @@ class Unet(nn.Module):
         self.layers.append(Conv3(128, 64))
         self.layers.append(Conv3(64, 64))
         self.layers.append(Conv1(64, out_ch))
-        
-        self.model = nn.Sequential(*self.layers)
-    
+
+        self.num_layers_list = [2, 3, 3, 3, 4, 3, 3, 3, 3]
+        assert sum(self.num_layers_list) == len(self.layers)
+
+        self.blocks = []
+        layer_i = 0
+        for num_layers in self.num_layers_list:
+            self.blocks.append(
+                nn.Sequential(*self.layers[layer_i : layer_i + num_layers])
+            )
+            layer_i += num_layers
+        assert layer_i == len(self.layers)
+        self.model = nn.Sequential(*self.blocks)
+
     def crop_and_concat(self, x: torch.Tensor, cached):
         hx, wx = x.shape[2:]
         y = cached.pop()
         hy, wy = y.shape[2:]
-        y_cropped = y[:, :, (hy-hx)//2:(hy-hx)//2+hx, (wy-wx)//2:(wy-wx)//2+wx]
-        assert y_cropped.shape == x.shape 
+        y_cropped = y[
+            :,
+            :,
+            (hy - hx) // 2 : (hy - hx) // 2 + hx,
+            (wy - wx) // 2 : (wy - wx) // 2 + wx,
+        ]
+        assert y_cropped.shape == x.shape
         x = torch.concat([y_cropped, x], dim=1)
-        return x 
+        return x
 
     def forward(self, x: torch.Tensor):
-        cached = [] 
-        for i in range(2):
-            layer = self.layers.pop(0)
-            x = layer(x)
-        cached.append(x)
-        for i in range(3):
-            layer = self.layers.pop(0)
-            x = layer(x)
-        cached.append(x)
-        for i in range(3):
-            layer = self.layers.pop(0)
-            x = layer(x)
-        cached.append(x)
-        for i in range(3):
-            layer = self.layers.pop(0)
-            x = layer(x)
-        cached.append(x)
-        
+        cached = []
         for i in range(4):
-            layer = self.layers.pop(0)
-            x = layer(x)
-        x = self.crop_and_concat(x, cached)
-        
-        for i in range(3):
-            layer = self.layers.pop(0)
-            x = layer(x)
-        x = self.crop_and_concat(x, cached)
-        
-        for i in range(3):
-            layer = self.layers.pop(0)
-            x = layer(x)
-        x = self.crop_and_concat(x, cached)
-        
-        for i in range(3):
-            layer = self.layers.pop(0)
-            x = layer(x)
-        x = self.crop_and_concat(x, cached)
-        
-        for i in range(3):
-            layer = self.layers.pop(0)
-            x = layer(x)
-         
-        assert not self.layers 
+            x = self.blocks[i](x)
+            cached.append(x)
+
+        for i in range(4, 8):
+            x = self.blocks[i](x)
+            x = self.crop_and_concat(x, cached)
+
+        assert not cached
+        x = self.blocks[-1](x)
+
         return x

2f252053c9103b5f344b6d9a199933913935e833
HJoonKwon <dsazz801@gmail.com>
2023-11-05 20:48:14 +0900

    update: add some cases for unit test

--- python/tests/test_unet.py
+++ python/tests/test_unet.py
@@ -9,3 +9,8 @@ def test_Unet():
     model = Unet(1, 2).to(device)
     output = model(input)
     assert output.shape == (1, 2, 512, 512)
+    
+    input = torch.randn((1, 1, 378, 378), device=device)
+    model = Unet(1, 2).to(device)
+    output = model(input)
+    assert output.shape == (1, 2, 378, 378)

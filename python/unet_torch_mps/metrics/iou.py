9584fde37d855198b8839f286ab80ac95bb8b3f5
HJoonKwon <dsazz801@gmail.com>
2023-11-18 22:30:44 +0900

    feat: implement new optimized miou calculation without using nn.module

--- python/unet_torch_mps/metrics/iou.py
+++ python/unet_torch_mps/metrics/iou.py
@@ -2,6 +2,38 @@ import torch
 from torchmetrics import JaccardIndex
 
 
+def calculate_mean_iou(preds, targets):
+    """
+    Mean Intersection over Union (mIoU) calculation.
+
+    Args:
+    preds (torch.Tensor): Predicted segmentation map, shape [N, C, H, W].
+    targets (torch.Tensor): Ground truth segmentation map, shape [N, H, W].
+
+    Returns:
+    float: Mean IoU score.
+    """
+
+    num_classes = preds.shape[1]
+    preds = torch.argmax(preds, dim=1).view(-1)
+    targets = targets.view(-1)
+
+    # Create confusion matrix
+    conf_matrix = torch.zeros((num_classes, num_classes), device=preds.device)
+    conf_matrix = conf_matrix.index_add_(
+        0, targets, torch.nn.functional.one_hot(preds, num_classes).float()
+    )
+
+    # IoU for each class
+    intersection = torch.diag(conf_matrix)
+    total = conf_matrix.sum(0) + conf_matrix.sum(1) - intersection
+    iou = intersection / total.clamp(min=1e-6)
+
+    # Mean IoU
+    miou = torch.mean(iou[total > 0])  # Mean over classes with non-zero total
+    return miou.item()
+
+
 class IoU(torch.nn.Module):
     def __init__(self, num_classes: int, task="multiclass", per_class=False):
         super().__init__()

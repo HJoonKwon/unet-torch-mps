711c18adc4716a1a3cb32a819908e8eda8193529
HJoonKwon <dsazz801@gmail.com>
2023-11-20 22:19:21 +0900

    fix: type bug

--- python/unet_torch_mps/loss/dice.py
+++ python/unet_torch_mps/loss/dice.py
@@ -8,7 +8,6 @@ class DiceLoss(nn.Module):
         super(DiceLoss, self).__init__()
         self.smooth = smooth
 
-    @torch.inference_mode()
     def forward(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
         """Calculate dice loss.
 
@@ -23,8 +22,15 @@ class DiceLoss(nn.Module):
             torch.int64,
             torch.int32,
         ), f"y_true.dtype: {y_true.dtype}"
+        if y_true.dtype == torch.int32:
+            y_true = y_true.long()
+        assert len(y_true.shape) == 3, f"y_true.shape: {y_true.shape}"
+
         y_pred = y_pred.softmax(dim=1)
         num_classes = y_pred.shape[1]
+        assert (
+            y_true.min() >= 0 and y_true.max() < num_classes
+        ), f"Invalid values in y_true: min {y_true.min()}, max {y_true.max()}"
         y_true_one_hot = F.one_hot(y_true, num_classes).permute(0, 3, 1, 2).float()
 
         intersection = torch.sum(y_pred * y_true_one_hot, dim=(2, 3))  # NXC

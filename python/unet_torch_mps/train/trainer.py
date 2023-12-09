804ebee2a1a18aa8bf3a4ad0123bb2ce45762d1a
HJoonKwon <dsazz801@gmail.com>
2023-12-09 22:47:29 +0900

    update: modularize Trainer. Utilize class atttirubtes e.g. model, logger, dataloader

--- python/unet_torch_mps/train/trainer.py
+++ python/unet_torch_mps/train/trainer.py
@@ -1,6 +1,3 @@
-import os
-import logging
-import argparse
 from tqdm import tqdm
 import torch
 from torch.nn.utils import clip_grad_norm_
@@ -36,44 +33,74 @@ class Trainer:
         self.logger.info(f"Trainer:: Using device: {self.device}")
         self.logger.info(f"Trainer:: Starting epoch: {self.start_epoch}")
 
-    def train_iter(self, img, mask_gt, model, optimizer, loss_fns: list, device):
-        img, mask_gt = img.to(device), mask_gt.to(device)
-        mask_pred_logit = model(img)
+    def train(self):
+        for epoch in range(self.start_epoch, self.num_epochs):
+            self.logger.info(f"Trainer:: Training epoch: {epoch}")
+            train_loss, train_miou = self.train_epoch()
+            self.logger.info(
+                f"Trainer:: Training epoch: {epoch} with train_loss: {train_loss} and train_miou: {train_miou}"
+            )
+            valid_loss, valid_miou = self.evaluate_epoch()
+            self.logger.info(
+                f"Trainer:: Training epoch: {epoch} with valid_loss: {valid_loss} and valid_miou: {valid_miou}"
+            )
+            self.save_best()
+
+    def train_iter(self, img, mask_gt):
+        mask_pred_logit = self.model(img)
         loss = None
-        for loss_fn in loss_fns:
+        for loss_fn in self.loss_fns:
             loss += loss_fn(mask_pred_logit, mask_gt)
-        optimizer.zero_grad()
+        self.optimizer.zero_grad()
         loss.backward()
-        clip_grad_norm_(model.parameters(), 1.0)
-        optimizer.step()
+        clip_grad_norm_(self.model.parameters(), 1.0)
+        self.optimizer.step()
         train_loss = loss.item()
         miou = calculate_mean_iou(mask_pred_logit, mask_gt)
         return train_loss, miou
 
-    def train_epoch(
-        self, model, train_loader, optimizer, loss_fns: list, device, logger
-    ):
-        model.train()
+    def train_epoch(self):
+        self.model.train()
         total_loss = 0
         total_miou = 0
-        for batch_idx, (img, mask_gt) in enumerate(train_loader):
+        for batch_idx, (img, mask_gt) in enumerate(self.train_loader):
             iter_loss, iter_miou = self.train_iter(
-                img, mask_gt, model, optimizer, loss_fns, device
+                img.to(self.device), mask_gt.to(self.device)
             )
-            logger.info(
-                f"training data({batch_idx}/{len(train_loader)}): iter loss: {iter_loss} and iter miou: {iter_miou}"
+            self.logger.info(
+                f"training data({batch_idx}/{len(self.train_loader)}): iter loss: {iter_loss} and iter miou: {iter_miou}"
             )
             total_loss += iter_loss
             total_miou += iter_miou
         avg_loss = total_loss / (batch_idx + 1)
         avg_miou = total_miou / (batch_idx + 1)
-        logger.info(
-            f"training data({batch_idx}/{len(train_loader)}): avg loss: {avg_loss} and avg miou: {avg_miou}"
-        )
         return avg_loss, avg_miou
 
-    def evaluate_iter(self):
-        pass
+    def evaluate_iter(self, img, mask_gt):
+        mask_pred_logit = self.model(img)
+        loss = None
+        for loss_fn in self.loss_fns:
+            loss += loss_fn(mask_pred_logit, mask_gt)
+        iter_loss = loss.item()
+        iter_miou = calculate_mean_iou(mask_pred_logit, mask_gt)
+        return iter_loss, iter_miou
 
     def evaluate_epoch(self):
-        pass
+        self.model.eval()  # set model to evaluation mode
+        with torch.no_grad():
+            total_loss = 0
+            total_miou = 0
+            for batch_idx, (img, mask_gt) in tqdm(
+                enumerate(self.valid_loader), total=len(self.valid_loader)
+            ):
+                iter_loss, iter_miou = self.evaluate_iter(
+                    img.to(self.device), mask_gt.to(self.device)
+                )
+                total_loss += iter_loss
+                total_miou += iter_miou
+            avg_miou = total_miou / (batch_idx + 1)
+            avg_loss = total_loss / (batch_idx + 1)
+        return avg_loss, avg_miou
+
+    def save_best(self):
+        pass 
